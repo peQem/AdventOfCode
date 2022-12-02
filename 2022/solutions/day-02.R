132047c73193923aa17480242398889db8755f54
peQem <40581567+peQem@users.noreply.github.com>
2022-12-02 23:10:15 +0100

    day-02.1

--- 2022/solutions/day-02.R
+++ 2022/solutions/day-02.R
@@ -5,7 +5,19 @@ day <- "02"
 input_file <- glue("input/day-{day}.txt")
 
 
-# keep appropriate alternative
-input <- read_file(input_file)
-input <- tibble(x = read_lines(input_file))
-input <- scan(input_file, sep = ",")
+input <-read_delim(input_file, delim = " ", col_names = c("opponent", "player"))
+
+
+x <- input %>% 
+  mutate(player = str_replace_all(player, c("X" = "A", "Y" = "B", "Z" = "C"))) %>% 
+  mutate(shape_score = str_replace_all(player, c("A" = "1", "B" = "2", "C" = "3")),
+         shape_score = as.numeric(shape_score)) %>% 
+  mutate(output_score = case_when(opponent == player ~ 3,
+                                  opponent == "A" & player == "B" ~ 6,
+                                  opponent == "B" & player == "C" ~ 6,
+                                  opponent == "C" & player == "A" ~ 6,
+                                  TRUE ~ 0
+  ))
+
+# solution part 1
+sum(x$shape_score, x$output_score)
