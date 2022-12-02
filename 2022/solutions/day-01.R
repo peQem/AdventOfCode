686391bdb6a327fd73d20efae6d642d212fef9c6
peQem <40581567+peQem@users.noreply.github.com>
2022-12-02 22:02:51 +0100

    day-01 lead()

--- 2022/solutions/day-01.R
+++ 2022/solutions/day-01.R
@@ -7,15 +7,16 @@ input_file <- glue("input/day-{day}.txt")
 
 input <- read_lines(input_file) %>% as.numeric()
 
-x <- c(input, 0) %>% replace_na(0) %>% cumsum()
 
-x <- x[x == c(x[-1], 0)]
+x <- c(input, NA) %>% replace_na(0) %>% cumsum()
 
-x <- c(x, 0) - c(0,x)
+x <- x[x == lead(x)]
+
+x <- c(x, 0) - c(0, x)
 
 
 # solution part 1
-max(x)
+max(x, na.rm = TRUE)
 
 # solution part 2
 sort(x) %>% tail(3) %>% sum()
