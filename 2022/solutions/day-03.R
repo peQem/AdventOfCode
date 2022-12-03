d74ab3e233d724b6aad5eedd4304a788b0fd44da
peQem <40581567+peQem@users.noreply.github.com>
2022-12-03 22:37:36 +0100

    day-03.1

--- 2022/solutions/day-03.R
+++ 2022/solutions/day-03.R
@@ -2,7 +2,25 @@ library(tidyverse)
 library(glue)
 
 day <- "03"
-input_file <- glue("../examples/day-{day}.txt")
+input_file <- glue("../input/day-{day}.txt")
 
 
-input <-read_lines(input_file)
+input <- read_lines(input_file) %>% tibble(input = .)
+
+
+
+# part 1
+priority <- set_names(c(1:52), c(letters, LETTERS))
+
+x <- input %>% 
+  mutate(compartment1 = substring(input, 1, nchar(input)/2),
+         compartment2 = substring(input, nchar(input)/2+1),
+         
+         items1 = strsplit(compartment1, ""),
+         items2 = strsplit(compartment2, ""),
+         
+         common_item = map2_chr(items1, items2, ~ intersect(.x, .y)),
+         priority = priority[common_item])
+
+# solution part 1
+sum(x$priority)
