55cf14498a1ab250970cd87433ce2fc2d88cedd5
peQem <40581567+peQem@users.noreply.github.com>
2022-12-04 23:26:29 +0100

    day-04.2

--- 2022/solutions/day-04.R
+++ 2022/solutions/day-04.R
@@ -17,9 +17,12 @@ x <- input %>%
          XX1 = map2(X11, X12, ~ seq(.x, .y)),
          XX2 = map2(X21, X22, ~ seq(.x, .y)),
 
-         O = map2(XX1, XX2, ~ intersect(.x, .y)),
+         overlap = map2(XX1, XX2, ~ intersect(.x, .y)),
 
-         fully_contain = map2_lgl(XX1, O, ~ length(.x) == length(.y)) | map2_lgl(XX2, O, ~ length(.x) == length(.y))) 
+         fully_contain = map2_lgl(XX1, overlap, ~ length(.x) == length(.y)) | map2_lgl(XX2, overlap, ~ length(.x) == length(.y))) 
 
 # solution part 1
 sum(x$fully_contain)
+
+# solution part 2
+sum(map_lgl(x$overlap, ~ length(.x) > 0))
