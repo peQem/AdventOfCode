9f6fc22dbfe145adf85a3feaf4a8875fee7038a4
peQem <40581567+peQem@users.noreply.github.com>
2022-12-05 20:38:50 +0100

    day-05.1

--- 2022/solutions/day-05.R
+++ 2022/solutions/day-05.R
@@ -8,4 +8,15 @@ input_file <- glue::glue("../examples/day-{day}.txt")
 
 
 # keep appropriate alternative
-input <- tibble(drawing = read_lines(input_file))
+input <- read_lines(input_file)
+
+change <- match("", input)
+
+stacks <- input[seq(change-2)] %>% 
+  strsplit("") %>% 
+  unlist() %>% 
+  matrix(nrow = (change-2), byrow = TRUE) %>% 
+  .[, seq(2, ncol(.), 4)] %>% 
+  na_if(" ")
+
+stacks <- rbind(matrix(NA, sum(!is.na(stacks)) - ncol(stacks), nrow(stacks)), stacks)
