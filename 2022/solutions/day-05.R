57429c3897858238a0aa320325d7f1d842c8d299
peQem <40581567+peQem@users.noreply.github.com>
2022-12-05 21:36:59 +0100

    day-05.1

--- 2022/solutions/day-05.R
+++ 2022/solutions/day-05.R
@@ -2,21 +2,47 @@ library(tidyverse)
 
 
 day <- "05"
-input_file <- glue::glue("../examples/day-{day}.txt")
+input_file <- glue::glue("../input/day-{day}.txt")
 
 
 
-
-# keep appropriate alternative
 input <- read_lines(input_file)
 
 change <- match("", input)
 
 stacks <- input[seq(change-2)] %>% 
-  strsplit("") %>% 
-  unlist() %>% 
-  matrix(nrow = (change-2), byrow = TRUE) %>% 
+  str_split("", simplify = TRUE) %>% 
   .[, seq(2, ncol(.), 4)] %>% 
   na_if(" ")
 
-stacks <- rbind(matrix(NA, sum(!is.na(stacks)) - ncol(stacks), nrow(stacks)), stacks)
+stacks <- rbind(matrix(NA, sum(!is.na(stacks)) - nrow(stacks), ncol(stacks)), stacks)
+
+procedure <- input[(change+1):length(input)] %>% 
+  str_split(" ", simplify = TRUE)
+
+new_stacks <- stacks
+
+
+
+# part 1
+for (p in seq(nrow(procedure))) {
+  
+  move <- procedure[p, 2] %>% as.integer()
+  from <- procedure[p, 4] %>%  as.integer()
+  to <- procedure[p, 6] %>% as.integer()
+  
+  for (m in seq(move)) {
+    
+    not_na <- colSums(is.na(new_stacks))+1
+    new_stacks[not_na[to]-1, to] <- new_stacks[not_na[from], from]
+    new_stacks[not_na[from], from] <- NA
+          
+  }
+
+}
+
+not_na <- colSums(is.na(new_stacks))+1 
+not_na <- cbind(not_na, seq(ncol(new_stacks)))
+
+# solution part 1
+new_stacks[not_na] %>% paste(collapse = "")
