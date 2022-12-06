0711d55a135620e18a6b6af7ca191a5bccd3312c
peQem <40581567+peQem@users.noreply.github.com>
2022-12-06 23:42:10 +0100

    day-06.2

--- 2022/solutions/day-06.R
+++ 2022/solutions/day-06.R
@@ -3,14 +3,14 @@ library(tidyverse)
 day <- "06"
 input_file <- glue::glue("../input/day-{day}.txt")
 
-input <- read_lines(input_file)
+input <- read_lines(input_file) %>% 
+  str_split("", simplify = TRUE) %>% 
+  t()
 
 
 
 # part 1
 x <- input %>%
-  str_split("", simplify = TRUE) %>% 
-  t() %>% 
   tibble(X1 = .) %>% 
   mutate(X2 = lag(X1),
          X3 = lag(X2),
@@ -18,6 +18,22 @@ x <- input %>%
 
 marker <- apply(x, 1, function(x) length(unique(na.omit(x))))
 
-
 # solution part 1
 which.max(marker == 4)
+
+
+
+# part 2
+window <- 14
+
+x <- runner::runner(
+  x = input,
+  k = window,
+  f = paste0,
+  collapse = ""
+)
+
+marker <- apply(str_split(x, "", simplify = TRUE), 1, function(z) length(unique(na.omit(z))))
+
+# solution part 2
+which.max(marker[seq(-1, -window)] == window) + window
